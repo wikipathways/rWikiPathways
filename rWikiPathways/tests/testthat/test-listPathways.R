97a711dd2ee4aee7017cecbf743c9ff4a37edf79
Egon Willighagen <egon.willighagen@gmail.com>
2015-12-20 14:57:25 +0100

    Fixed the test description

--- rWikiPathways/tests/testthat/test-listPathways.R
+++ rWikiPathways/tests/testthat/test-listPathways.R
@@ -6,7 +6,7 @@ test_that("pathways are found", {
   expect_that(nrow(organisms), not(equals(0)))
 })
 
-test_that("human pathways are found", {
+test_that("Anopheles gambiae pathways are found", {
   organisms = listPathways(organism="Anopheles gambiae")
   expect_that(nrow(organisms), not(equals(0)))
 })
