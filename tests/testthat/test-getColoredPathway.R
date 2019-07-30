e22f5609a1fa4cea0410ba87f7bde430d99526c2
Alexander Pico <apico@gladstone.ucsf.edu>
2019-07-30 11:57:34 -0700

    bug fix: Removed tests for unavailable service

--- tests/testthat/test-getColoredPathway.R
+++ tests/testthat/test-getColoredPathway.R
@@ -1,19 +1,21 @@
 library(rWikiPathways)
 context("Pathway")
 
-test_that("colored pathway", {
-  svg = getColoredPathway(pathway="WP1842", graphId=c("dd68a","a2c17"));
-  expect_gt(nchar(svg), 0)
-})
+# getColoredPathway not available
 
-test_that("green colored pathway", {
-  svg = getColoredPathway(pathway="WP1842", graphId=c("dd68a","a2c17"),
-    color="00FF00");
-  expect_gt(nchar(svg), 0)
-})
-
-test_that("colored pathway (vector)", {
-  svg = getColoredPathway(pathway="WP1842", graphId=c("dd68a","a2c17"),
-    color=c("FF0000", "00FF00"));
-  expect_gt(nchar(svg), 0)
-})
+# test_that("colored pathway", {
+#   svg = getColoredPathway(pathway="WP1842", graphId=c("dd68a","a2c17"));
+#   expect_gt(nchar(svg), 0)
+# })
+# 
+# test_that("green colored pathway", {
+#   svg = getColoredPathway(pathway="WP1842", graphId=c("dd68a","a2c17"),
+#     color="00FF00");
+#   expect_gt(nchar(svg), 0)
+# })
+# 
+# test_that("colored pathway (vector)", {
+#   svg = getColoredPathway(pathway="WP1842", graphId=c("dd68a","a2c17"),
+#     color=c("FF0000", "00FF00"));
+#   expect_gt(nchar(svg), 0)
+# })
