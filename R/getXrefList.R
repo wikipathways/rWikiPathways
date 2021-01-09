faddeeac9c57bc8dde6e9bca8b75dfd4efa003f7
Egon Willighagen <egon.willighagen@gmail.com>
2021-01-09 11:02:38 +0100

    Updated the URL to the BridgeDb datasources file (new folder, new extension)

--- R/getXrefList.R
+++ R/getXrefList.R
@@ -5,7 +5,7 @@
 #' @param pathway WikiPathways identifier (WPID) for the pathway to download, e.g. WP4
 #' @param systemCode (\code{character}) The BridgeDb code associated with the data source or system, 
 #' e.g., En (Ensembl), L (Entrez), Ch (HMDB), etc.
-#' See column two of https://github.com/bridgedb/BridgeDb/blob/master/org.bridgedb.bio/resources/org/bridgedb/bio/datasources.txt.
+#' See column two of https://github.com/bridgedb/BridgeDb/blob/master/org.bridgedb.bio/src/main/resources/org/bridgedb/bio/datasources.tsv.
 #' @return A \code{list} of Xrefs identifiers
 #' @examples {
 #' xrefs = getXrefList("WP2338", "L")
