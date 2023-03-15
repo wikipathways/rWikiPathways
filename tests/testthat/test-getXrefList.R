2107f348da61a2642ee06fc47202e11b7c45f09d
Egon Willighagen <egon.willighagen@gmail.com>
2023-03-15 20:14:03 +0100

    Updated the test to match the webservice call results

--- tests/testthat/test-getXrefList.R
+++ tests/testthat/test-getXrefList.R
@@ -3,10 +3,10 @@ context("Pathway")
 
 test_that("get WP2338 Xrefs", {
   xrefs = getXrefList("WP2338", "S")
-  expect_true("Q9UPY3" %in% xrefs)
+  expect_true("Q15633" %in% xrefs)
 })
 
 test_that("get WP2338 (again)", {
   xrefs = getXrefList(pathway="WP2338", systemCode="S")
-  expect_true("Q9UPY3" %in% xrefs)
+  expect_true("Q15633" %in% xrefs)
 })
